f4f947c6997a7bbe10c3aac8427d6e6296b36514
Leonardo-Rozza <leonardo155964@gmail.com>
2024-10-18 14:54:37 -0300

    Empezando con el DOM

---
+++ js/09-DOM/01-inicial-dom.js
@@ -0,0 +1,10 @@
+let elemento = document.head;
+
+let elemento2 = document.forms[0].name;
+
+console.log(elemento2)
+
+
+let elementoP = document.getElementById("importantes");
+
+elementoP.innerText = "Estoy cambiando el texto desde JS";
